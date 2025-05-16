8e89f164d15bca6f1cf969b951c6253f855f813c
adarshmalviy <adarshmalvi77@gmail.com>
2025-05-16 19:12:44 +0530

    Add setup for pip installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='microalerts',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['requests'],
+    author='Adarsh Malviya',
+    description='Microservice notification library for sending alerts to Slack and other platforms.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
